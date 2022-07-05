1ddacf72e1abbc7d9f14b7b7eb5f3b69c3976935
limao <limao2021@bytedance.com>
2022-07-05 11:45:39 +0800

    feat: test arco

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
 import type { NextPage } from "next";
 import { useTransition } from "react";
+import {Button} from '@arco-design/web-react'
 import styles from "../styles/Home.module.css";
+import '@arco-design/web-react/dist/css/arco.css';
 
 const Home: NextPage = () => {
   useTransition();
-  return <div className={styles.container}></div>;
+  return <div className={styles.container}>
+    <Button type='secondary'>Test Arco</Button>
+  </div>;
 };
 
 export default Home;
