1243d38bdd6e636e6833a48f5d9f7f7a7893af5d
limao <limao2021@bytedance.com>
2022-07-05 11:50:33 +0800

    feat: test i18n

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,15 @@
 import type { NextPage } from "next";
-import { useTransition } from "react";
 import {Button} from '@arco-design/web-react'
 import styles from "../styles/Home.module.css";
 import '@arco-design/web-react/dist/css/arco.css';
+import './i18n';
+import { useTranslation } from 'react-i18next';
 
 const Home: NextPage = () => {
-  useTransition();
+  const { t, i18n } = useTranslation();
   return <div className={styles.container}>
     <Button type='secondary'>Test Arco</Button>
+    <div>{t('Welcome to React')}</div>
   </div>;
 };
 
