30e3ef236474b239cdf0de17622830bdbd90a483
limao <limao2021@bytedance.com>
2022-08-15 15:42:14 +0800

    feat: request

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,57 +1,17 @@
-import type { NextPage } from "next";
-import { Button, Image } from "@arco-design/web-react";
-import "@arco-design/web-react/dist/css/arco.css";
 import { useEffect, useState, useTransition } from "react";
-import { randImg } from "@ngneat/falso";
+import useSWR from 'swr';
 
-const sleep = (time: number) =>
-  new Promise((resolve) => setTimeout(resolve, time));
-
-const mockApi = async (id: number) => {
-  console.warn("mock api send", id);
-  sleep(1000);
-  return await `mock api ${id}`;
-};
-
-// const getImg = async () => {
-//   await sleep(1000);
-//   return await new Promise<string>((resolve) => {
-//     resolve(randImg());
-//   });
-// };
-
-export async function getStaticProps(context: any) {
-  console.log("getStaticProps", context);
-  const data = await mockApi(1);
-  return {
-    props: { data }, // will be passed to the page component as props
-  };
-}
+const fetcher = (url: string) => fetch(url).then((res) => res.json())
 
 const App = (props = {}) => {
-  const [value, setValue] = useState("");
-  const [isPending, startTransition] = useTransition();
-//   const onClick = async () => {
-//     const img = await getImg();
-//     console.log(img);
-//     startTransition(() => {
-//       setValue(img);
-//     });
-//     console.log("process", process);
-//   };
+  const { data, error } = useSWR('/api/hello', fetcher)
+  
   useEffect(() => {
     console.log("render");
     console.log("props", props);
   });
 
-  const req = async () => {
-    console.log("req");
-    const res = await mockApi(2);
-    setValue(res);
-  };
-
   useEffect(() => {
-    req();
   }, []);
 
   return (
@@ -59,7 +19,7 @@ const App = (props = {}) => {
       {/* <Button onClick={onClick}>hello react 18</Button> */}
       {/* {isPending ? "loading..." : <Image src={value} />} */}
       {(props as any).data }
-      {value}
+      {data ? JSON.stringify(data) : 'loading'}
     </div>
   );
 };
