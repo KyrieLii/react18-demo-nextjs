2e2be232dc950ea7da716630713669d9cfd85c35
limao <limao2021@bytedance.com>
2022-08-15 14:58:27 +0800

    feat: mock api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,67 @@
 import type { NextPage } from "next";
-import { Button, Image } from '@arco-design/web-react';
-import '@arco-design/web-react/dist/css/arco.css';
-import { useState, useTransition } from 'react';
-import { randImg } from '@ngneat/falso';
+import { Button, Image } from "@arco-design/web-react";
+import "@arco-design/web-react/dist/css/arco.css";
+import { useEffect, useState, useTransition } from "react";
+import { randImg } from "@ngneat/falso";
 
-const sleep = (time: number) => new Promise((resolve) => setTimeout(resolve, time));
+const sleep = (time: number) =>
+  new Promise((resolve) => setTimeout(resolve, time));
 
-const getImg = async () => {
-    await sleep(1000);
-    return await new Promise<string>((resolve) => {
-        resolve(randImg());
-    })
+const mockApi = async (id: number) => {
+  console.warn("mock api send", id);
+  sleep(1000);
+  return await `mock api ${id}`;
+};
+
+// const getImg = async () => {
+//   await sleep(1000);
+//   return await new Promise<string>((resolve) => {
+//     resolve(randImg());
+//   });
+// };
+
+export async function getStaticProps(context: any) {
+  console.log("getStaticProps", context);
+  const data = await mockApi(1);
+  return {
+    props: { data }, // will be passed to the page component as props
+  };
 }
 
-const App = () => {
-  const [value, setValue] = useState('');
+const App = (props = {}) => {
+  const [value, setValue] = useState("");
   const [isPending, startTransition] = useTransition();
-  const onClick = async () => {
-    const img = await getImg();
-    console.log(img)
-    startTransition(() => {
-      setValue(img);
-    });
-    console.log('process', process)
+//   const onClick = async () => {
+//     const img = await getImg();
+//     console.log(img);
+//     startTransition(() => {
+//       setValue(img);
+//     });
+//     console.log("process", process);
+//   };
+  useEffect(() => {
+    console.log("render");
+    console.log("props", props);
+  });
+
+  const req = async () => {
+    console.log("req");
+    const res = await mockApi(2);
+    setValue(res);
   };
+
+  useEffect(() => {
+    req();
+  }, []);
+
   return (
     <div>
-      <Button onClick={onClick}>hello react 18</Button>
-      {isPending ?   'loading...': <Image src={value} />}
+      {/* <Button onClick={onClick}>hello react 18</Button> */}
+      {/* {isPending ? "loading..." : <Image src={value} />} */}
+      {(props as any).data }
+      {value}
     </div>
   );
 };
 
 export default App;
-
